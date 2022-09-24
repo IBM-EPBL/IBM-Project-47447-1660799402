690b4265940ee4d75c25c96229fbe1cc3f1ce93a
sharathykabilan <113613942+sharathykabilan@users.noreply.github.com>
2022-09-24 15:10:31 +0530

    Add files via upload

---
+++ ibm.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the temperature value:"))
+b=int(input("Enter the humidity value:"))
+if(a>37):
+
+    print("high temperature so the alarm being detected")
+
+else:
+
+    print("Low temperature so the alarm not detected")
