908a4274e6b513da49b27c105fde0842a0a92158
sharathykabilan <113613942+sharathykabilan@users.noreply.github.com>
2022-11-06 11:28:23 +0530

    Delete ibm.py

--- ibm.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter the temperature value:"))
-b=int(input("Enter the humidity value:"))
-if(a>37):
-
-    print("high temperature so the alarm being detected")
-
-else:
-
-    print("Low temperature so the alarm not detected")
